869b167921b3fa74857a8a5a1dfd3a782cb739fc
Diogo Cardoso <94764449+Cordelios810@users.noreply.github.com>
2022-03-24 11:03:30 +0000

    Update Rules.h

--- GAME/Rules.h
+++ GAME/Rules.h
@@ -1,8 +1,4 @@
-#include "pitches.h"
-
-
-
-
+//notas da musica
 int melodyRules[] = {
   NOTE_G4, 
   NOTE_A4, 
@@ -70,23 +66,23 @@ int melodyRules[] = {
   
 };
 
-
+//tempo de duração de cada nota
 int noteDurationsRules[] = {
  
 8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,4,4,4,1,};
 
 int Rules() {
-  
+  //Numero de notas a serem tocadas
   for (int thisNoteRules = 0; thisNoteRules < 50; thisNoteRules++) {
 
-  
+  //Calculo para obter o tempo que a nota vai durar
     int noteDurationRules = 1000 / noteDurationsRules[thisNoteRules];
     tone(3, melodyRules[thisNoteRules], noteDurationRules);
 
-   
+   //Pausa entre notas
     int pauseBetweenNotesRules = noteDurationRules * 1;
     delay(pauseBetweenNotesRules);
-    
+    //Pin do buzzer
     noTone(3);
     
   }
