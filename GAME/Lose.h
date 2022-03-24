921018bd4a00e21bdd0d6e6d843aac98cf40bea5
Diogo Cardoso <94764449+Cordelios810@users.noreply.github.com>
2022-03-24 11:03:35 +0000

    Update Lose.h

--- GAME/Lose.h
+++ GAME/Lose.h
@@ -1,3 +1,4 @@
+//notas da musica
 int melodyLose[] = {
   NOTE_C5,  //1
   NOTE_G4,  //2
@@ -16,7 +17,7 @@ int melodyLose[] = {
 
 };
 
-
+//tempo de duração de cada nota
 int noteDurationsLose[] = {
 
   4,
@@ -34,17 +35,17 @@ int noteDurationsLose[] = {
 };
 
 int Lose() {
-
+//Numero de notas a serem tocadas
   for (int thisNote = 0; thisNote < 12; thisNote++) {
 
-
+   //Calculo para obter o tempo que a nota vai durar
     int noteDurationLose = 1000 / noteDurationsLose[thisNote];
     tone(3, melodyLose[thisNote], noteDurationLose);
 
-
+    //Pausa entre notas
     int pauseBetweenNotes = noteDurationLose * 1;
     delay(pauseBetweenNotes);
-
+    //Pin do buzzer
     noTone(3);
   }
 }
