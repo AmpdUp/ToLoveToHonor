0b2be342912693183943044c2674f6526e144a33
PedroTorrado <pedro.espanha.torrado@gmail.com>
2022-04-07 01:04:28 +0100

    Updated Project Soldered Board

--- GAME/GAME.ino
+++ GAME/GAME.ino
@@ -4,46 +4,45 @@
 #include "Lose.h"
 #include "Power_UP.h"
 
-#define BlueLed 13
-#define GreenLed 12
-#define RedLed 11
-#define PurpleLed 10
+#define PurpleLed 13
+#define RedLed 12
+#define GreenLed 11
+#define BlueLed 10
 
 #define LedGame0 A1
 #define LedGame1 8
 #define LedGame2 A2
 #define LedGame3 9
 
-#define BlueButton 7
-#define GreenButton 6
-#define RedButton 5
-#define PurpleButton 4
+#define PurpleButton 7
+#define RedButton 6
+#define GreenButton 5
+#define BlueButton 4
 
 #define RLed A3
 #define BLed A4
 #define GLed A5
 
-int ON = 0;
-int OFF = 1;
+int ON = 1;
+int OFF = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
- 
+
   pinMode(BlueLed, OUTPUT);
   pinMode(GreenLed, OUTPUT);
   pinMode(RedLed, OUTPUT);
   pinMode(PurpleLed, OUTPUT);
-  
+
   pinMode(RLed, OUTPUT);
   pinMode(BLed, OUTPUT);
   pinMode(GLed, OUTPUT);
-    
+
   pinMode(BlueButton, INPUT);
   pinMode(GreenButton, INPUT);
   pinMode(RedButton, INPUT);
   pinMode(PurpleButton, INPUT);
-  
+
   pinMode(LedGame0, OUTPUT);
   pinMode(LedGame1, OUTPUT);
   pinMode(LedGame2, OUTPUT);
@@ -60,149 +59,145 @@ void setup()
   RuleDefining();
 }
 
-void loop()
-{     
+void loop() {
   bool Blue = digitalRead(BlueButton);
   bool Green = digitalRead(GreenButton);
   bool Red = digitalRead(RedButton);
   bool Purple = digitalRead(PurpleButton);
-	
+
   int DancyDance();
   int Cry();
- 
+
   value();
   String ColorState = value();
   //Serial.println(ColorState);
-  
-  if (Blue == 1){
-      if(ColorState == "Blue"){
-        Serial.println("Win");
-        delay(500);
-        DancyDance();
-      }
-      else{
-        Serial.println("Lose");
-        delay(500);
-        Cry();
-      }
+
+  if (Blue == 1) {
+    if (ColorState == "Blue") {
+      Serial.println("Win");
+      delay(500);
+      DancyDance();
+    } else {
+      Serial.println("Lose");
+      delay(500);
+      Cry();
+    }
   }
 
-  else if (Green == 1){
-      if(ColorState == "Green"){
-        Serial.println("Win");
-        delay(500);
-        DancyDance();
-      }
-      else{
-        Serial.println("Lose");
-        delay(500);
-        Cry();
-      }
+  else if (Green == 1) {
+    if (ColorState == "Green") {
+      Serial.println("Win");
+      delay(500);
+      DancyDance();
+    } else {
+      Serial.println("Lose");
+      delay(500);
+      Cry();
+    }
   }
 
-  else if (Red == 1){
-      if(ColorState == "Red"){
-        Serial.println("Win");
-        delay(500);
-        DancyDance();
-      }
-      else{
-        Serial.println("Lose");
-        delay(500);
-        Cry();
-      }
+  else if (Red == 1) {
+    if (ColorState == "Red") {
+      Serial.println("Win");
+      delay(500);
+      DancyDance();
+    } else {
+      Serial.println("Lose");
+      delay(500);
+      Cry();
+    }
   }
 
-  else if (Purple == 1){
-      if(ColorState == "Purple"){
-        Serial.println("Win");
-        delay(500);
-        DancyDance();
-      }
-      else{
-        Serial.println("Lose");
-        delay(500);
-        Cry();
-      }
+  else if (Purple == 1) {
+    if (ColorState == "Purple") {
+      Serial.println("Win");
+      delay(500);
+      DancyDance();
+    } else {
+      Serial.println("Lose");
+      delay(500);
+      Cry();
+    }
   }
 }
 
 int NewMillis = 0;
-int val = 7;
+int val = 0;
 
-String value(){
+String value() {
 
   int CurrentMillis = millis();
   int WaitingTime = 500;
 
   randomSeed(analogRead(0));
 
-  if (CurrentMillis - NewMillis >= WaitingTime){
-    val = random(0,7+1);
+  if (CurrentMillis - NewMillis >= WaitingTime) {
+    val = random(0, 6);
     NewMillis = CurrentMillis;
+    //Serial.println(val);
   }
 
   String Color;
-  
-  switch(val){
 
-    case 1 :  
+  switch (val) {
+
+    case 0:
       Color = "Red";
       digitalWrite(RLed, ON);
       digitalWrite(GLed, OFF);
       digitalWrite(BLed, OFF);
       break;
 
-    case 2 :  
+    case 1:
       Color = "Purple";
       digitalWrite(RLed, ON);
       digitalWrite(GLed, OFF);
       digitalWrite(BLed, ON);
       break;
 
-    case 3 :  
+    case 2:
       Color = "Blue";
       digitalWrite(RLed, OFF);
       digitalWrite(GLed, OFF);
       digitalWrite(BLed, ON);
       break;
 
-    case 4 :  
+    case 3:
       Color = "Cyan";
       digitalWrite(RLed, OFF);
       digitalWrite(GLed, ON);
       digitalWrite(BLed, ON);
       break;
-    case 5 :
+    case 4:
       Color = "Green";
       digitalWrite(RLed, OFF);
       digitalWrite(GLed, ON);
       digitalWrite(BLed, OFF);
       break;
 
-    case 6 : 
+    case 5:
       Color = "Yellow";
       digitalWrite(RLed, ON);
       digitalWrite(GLed, ON);
       digitalWrite(BLed, OFF);
       break;
 
-    case 7 : 
+    case 6:
       Color = "White";
       digitalWrite(RLed, ON);
       digitalWrite(GLed, ON);
       digitalWrite(BLed, ON);
       break;
 
-    default :    
+    default:
       Color = "Value not Supported";
       break;
   }
   return Color;
 }
 
-int DancyDance(){
-    
+int DancyDance() {
+
   digitalWrite(BlueLed, OFF);
   digitalWrite(GreenLed, OFF);
   digitalWrite(RedLed, OFF);
@@ -213,21 +208,20 @@ int DancyDance(){
   int Win();
   Win();
 
-  while(end != 0){
-       
+  while (end != 0) {
+
     int CurrentMillis = millis();
     int WaitingTime = 500;
 
-    if(CurrentMillis - NewMillis >= WaitingTime){
-      if(state == 1){
+    if (CurrentMillis - NewMillis >= WaitingTime) {
+      if (state == 1) {
         digitalWrite(BlueLed, ON);
         digitalWrite(GreenLed, ON);
         digitalWrite(RedLed, ON);
         digitalWrite(PurpleLed, ON);
         state = 0;
         Serial.println("ON");
-      }
-      else{
+      } else {
         digitalWrite(BlueLed, OFF);
         digitalWrite(GreenLed, OFF);
         digitalWrite(RedLed, OFF);
@@ -236,19 +230,15 @@ int DancyDance(){
         Serial.println("OFF");
       }
       NewMillis = CurrentMillis;
-  	}
-    
+    }
+
     bool Blue = digitalRead(BlueButton);
     bool Green = digitalRead(GreenButton);
     bool Red = digitalRead(RedButton);
     bool Purple = digitalRead(PurpleButton);
-    
-    if(
-      Blue == 1 or
-      Green == 1 or
-      Red == 1 or
-      Purple == 1
-    ){
+
+    if (
+      Blue == 1 or Green == 1 or Red == 1 or Purple == 1) {
       end = 0;
       Serial.println("Resume");
       delay(500);
@@ -256,15 +246,16 @@ int DancyDance(){
       digitalWrite(GreenLed, ON);
       digitalWrite(RedLed, ON);
       digitalWrite(PurpleLed, ON);
+    } else {
+      end = 1;
     }
-    else{end = 1;}
   }
-  RuleDefining();
   int Rules();
   Rules();
+  RuleDefining();
 }
 
-int Cry(){
+int Cry() {
   digitalWrite(BlueLed, OFF);
   digitalWrite(GreenLed, OFF);
   digitalWrite(RedLed, OFF);
@@ -275,21 +266,20 @@ int Cry(){
   int Lose();
   Lose();
 
-  while(end != 0){
-       
+  while (end != 0) {
+
     int CurrentMillis = millis();
     int WaitingTime = 1000;
 
-  	if(CurrentMillis - NewMillis >= WaitingTime){
-      if(state == 1){
+    if (CurrentMillis - NewMillis >= WaitingTime) {
+      if (state == 1) {
         digitalWrite(BlueLed, ON);
         digitalWrite(GreenLed, ON);
         digitalWrite(RedLed, ON);
         digitalWrite(PurpleLed, ON);
         state = 0;
         Serial.println("ON");
-      }
-      else{
+      } else {
         digitalWrite(BlueLed, OFF);
         digitalWrite(GreenLed, OFF);
         digitalWrite(RedLed, OFF);
@@ -298,19 +288,15 @@ int Cry(){
         Serial.println("OFF");
       }
       NewMillis = CurrentMillis;
-  	}
-    
+    }
+
     bool Blue = digitalRead(BlueButton);
     bool Green = digitalRead(GreenButton);
     bool Red = digitalRead(RedButton);
     bool Purple = digitalRead(PurpleButton);
-    
-    if(
-      Blue == 1 or
-      Green == 1 or
-      Red == 1 or
-      Purple == 1
-    ){
+
+    if (
+      Blue == 1 or Green == 1 or Red == 1 or Purple == 1) {
       end = 0;
       Serial.println("Resume");
       delay(500);
@@ -318,32 +304,34 @@ int Cry(){
       digitalWrite(GreenLed, ON);
       digitalWrite(RedLed, ON);
       digitalWrite(PurpleLed, ON);
+    } else {
+      end = 1;
     }
-    else{end = 1;}
   }
-  RuleDefining();
   int Rules();
   Rules();
+  RuleDefining();
 }
 
-int RuleDefining(){
+int RuleDefining() {
 
   randomSeed(analogRead(0));
-  
+
   Serial.println();
-  
+
   int bit0 = random(0, 2);
   int bit1 = random(0, 2);
   int bit2 = random(0, 2);
   int bit3 = random(0, 2);
-  /*
+  
   Serial.println(bit0);
   Serial.println(bit1);
   Serial.println(bit2);
   Serial.println(bit3);
-  */  
-  digitalWrite(LedGame0, bit0);
+  
   digitalWrite(LedGame1, bit1);
   digitalWrite(LedGame2, bit2);
   digitalWrite(LedGame3, bit3);
+  digitalWrite(LedGame0, bit0);
+
 }
--- GAME/Rules.h
+++ GAME/Rules.h
@@ -1,45 +1,4 @@
-//notas da musica
 int melodyRules[] = {
-  NOTE_G4, 
-  NOTE_A4, 
-  NOTE_B4, 
-  NOTE_CS5, 
-  
-  NOTE_G4, 
-  NOTE_A4, 
-  NOTE_B4,
-  NOTE_CS5,
-
-  NOTE_GS4,
-  NOTE_AS4,
-  NOTE_C5,
-  NOTE_D5,
-
-  NOTE_GS4,
-  NOTE_AS4,
-  NOTE_C5,
-  NOTE_D5,
-
-  NOTE_A4,
-  NOTE_B4,
-  NOTE_CS5,
-  NOTE_DS5,
-
-  NOTE_AS4,
-  NOTE_C5,
-  NOTE_D5,
-  NOTE_E5,
-
- NOTE_AS4,
-  NOTE_C5,
-  NOTE_D5,
-  NOTE_E5,
-  
-  NOTE_B4,
-  NOTE_CS5,
-  NOTE_DS5,
-  NOTE_F5,
-
   NOTE_C5,
   NOTE_D5,
   NOTE_E5,
@@ -63,17 +22,16 @@ int melodyRules[] = {
   NOTE_B5,
   NOTE_C6,
 
-  
 };
 
 //tempo de duração de cada nota
 int noteDurationsRules[] = {
  
-8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,4,4,4,1,};
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,4,4,4,1,};
 
 int Rules() {
   //Numero de notas a serem tocadas
-  for (int thisNoteRules = 0; thisNoteRules < 50; thisNoteRules++) {
+  for (int thisNoteRules = 0; thisNoteRules < 18; thisNoteRules++) {
 
   //Calculo para obter o tempo que a nota vai durar
     int noteDurationRules = 1000 / noteDurationsRules[thisNoteRules];
@@ -84,6 +42,6 @@ int Rules() {
     delay(pauseBetweenNotesRules);
     //Pin do buzzer
     noTone(3);
-    
+
   }
 }
